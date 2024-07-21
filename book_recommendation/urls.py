5b69695cde042ba05f34bfd77e4ac31bde794a54
amir <amir1378khalili@gmail.com>
2024-07-21 22:47:55 +0330

    ADD accounts and book urls

--- book_recommendation/urls.py
+++ book_recommendation/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/accounts/", include("accounts.urls", namespace="accounts")),
+    path("api/", include("accounts.urls", namespace="accounts")),
+    path("api/", include("book.urls", namespace="book")),
 ]
