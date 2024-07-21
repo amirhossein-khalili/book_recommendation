14c89c26b70d7ff4780a7f03b80ba4a3fb0a7b37
amir <amir1378khalili@gmail.com>
2024-07-21 22:48:07 +0330

    ADD book urls

---
+++ book/urls.py
@@ -0,0 +1,31 @@
+from django.urls import include, path
+
+from . import views
+
+app_name = "book"
+
+
+# ----------------------------------------------------------------
+# -------------------     BOOK PART OF URLS         --------------
+# ----------------------------------------------------------------
+book_urls = [
+    path("list/", views.BookListView.as_view(), name="book-list"),
+    path("<int:pk>/", views.BookDetailView.as_view(), name="book-detail"),
+    path(
+        "genre/<str:genre>/", views.BookRecommendView.as_view(), name="book-recommend"
+    ),
+]
+
+# ----------------------------------------------------------------
+# -------------------     REVIEW PART OF URLS         ------------
+# ----------------------------------------------------------------
+review_urls = [
+    path("add/", views.ReviewAddView.as_view(), name="review-add"),
+    path("update/<int:pk>/", views.ReviewUpdateView.as_view(), name="review-update"),
+    path("delete/<int:pk>/", views.ReviewDeleteView.as_view(), name="review-delete"),
+]
+
+urlpatterns = [
+    path("book/", include(book_urls)),
+    path("review/", include(review_urls)),
+]
