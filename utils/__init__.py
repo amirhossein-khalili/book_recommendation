e12c6392f4be04c8d21129777173fe9bfe6d1701
amir <amir1378khalili@gmail.com>
2024-07-23 00:47:32 +0330

    ADD extract_values_list_dicts

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from .code_generator import code_generator
+from .extract_values_list_dicts import extract_values_list_dicts
 from .redis_client import redis_instance
 from .remove_duplicates import remove_duplicates
