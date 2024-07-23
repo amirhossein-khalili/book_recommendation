7b97dbc055ff8f0cdde110507fc829fbdd8d2cb7
amir <amir1378khalili@gmail.com>
2024-07-23 05:00:59 +0330

    ADD swagger path

--- book_recommendation/urls.py
+++ book_recommendation/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls", namespace="accounts")),
     path("api/", include("book.urls", namespace="book")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
