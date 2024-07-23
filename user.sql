7ec23bc0552f90dd62b984121da734d5ec5eb34f
amir <amir1378khalili@gmail.com>
2024-07-23 04:07:58 +0330

    ADD user sql

---
+++ user.sql
@@ -0,0 +1,6 @@
+INSERT INTO accounts_user (user_name, password, Email,is_superuser,is_active,is_admin , Phone_number) VALUES
+('user1', 'password1', 'user1@gmail.com',False ,True ,False , '091111111'),
+('user2', 'password2', 'user2@gmail.com',False ,True ,False , '091111112'),
+('user3', 'password3', 'user3@gmail.com',False ,True ,False , '091111113'),
+('user4', 'password4', 'user4@gmail.com',False ,True ,False , '091111114'),
+('user5', 'password5', 'user5@gmail.com',False ,True ,False , '091111115');
